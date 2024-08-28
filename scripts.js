066587394b305a7a0b8049a991cf7dc46c43a67a
joaolucas14 <jlparceiro10@gmail.com>
2024-08-28 19:04:07 -0300

    criando a funcionalidade das tags junto com suas verificações

--- scripts.js
+++ scripts.js
@@ -35,3 +35,48 @@ inputUpload.addEventListener('change', async (evento) =>{
         }
     }
 })
+
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
+
+
+
+listaTags.addEventListener("click", (evento) =>{
+    if (evento.target.classList.contains("remove-tag")){
+        const tagQueQueremosRemover = evento.target.parentElement;
+        listaTags.removeChild(tagQueQueremosRemover)
+    }
+})
+
+const tagsDisponiveis = ["Front-end", "Programação", "Data Science", "Full-stack", "HTML", "CSS", "JavaScript"]
+
+async function verificaTagsDisponiveis(tagTexto){
+    return new Promise((resolve) =>{
+        setTimeout(()=>{
+            resolve(tagsDisponiveis.includes(tagTexto));
+        }, 1000)
+    })
+}
+
+inputTags.addEventListener('keypress', async (evento) =>{
+    if (evento.key === "Enter"){
+        evento.preventDefault()
+        const tagTexto = inputTags.value.trim();
+        if (tagTexto !== ""){
+            try{
+                const tagExiste = await verificaTagsDisponiveis(tagTexto)
+                if (tagExiste){
+                const tagNova = document.createElement("li");
+                tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+                listaTags.appendChild(tagNova);
+                inputTags.value =""}
+                else{
+                    alert("Tag não foi encontrada")
+                }}
+            catch (erro){
+                console.error("Erro ao verificar a existencia da tag")
+                alert("Erro ao verificar a existencia da tag")
+            }
+        }
+    }
+})
